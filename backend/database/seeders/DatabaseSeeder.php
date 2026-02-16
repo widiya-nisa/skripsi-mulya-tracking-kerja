e2ac30b20a4e1a075c1d82d6e4d523069a339114
Developer <developer@trackingkerja.com>
2026-02-16 11:49:06 +0700

    Tambahkan DepartmentSeeder ke DatabaseSeeder untuk otomatis isi data Job Description

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            DepartmentSeeder::class,  // Tambahkan ini untuk mengisi data Job Description
             AdminSeeder::class,
         ]);
     }
