b6c651fe6d2cd352de67dbf61a9b371345c1b740
Developer <developer@trackingkerja.com>
2026-02-17 19:00:48 +0700

    fixing enpoint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "/api";
+const API_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:8000/api";
 
 // Create axios instance
 const api = axios.create({
